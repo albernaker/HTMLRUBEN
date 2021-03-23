e3e86881d72b87ed79c41cb85d4dd65fe5f60b6d
albernaker <albert.mora@mataro.epiaedu.cat>
2021-03-23 15:20:07 +0100

    commit

--- ActivitatsPHP/UF2_AC1/uytransfer/upload.php
+++ ActivitatsPHP/UF2_AC1/uytransfer/upload.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>PHP</title>
-		<link rel="stylesheet" type="text/css" href="CSS/estilos.css" />
+		<link rel="stylesheet" type="text/css" href="css/estilos.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<body>
@@ -13,36 +13,59 @@
 
 include "header.php";
 
-print_r($_POST);
-print_r($_FILES);
 
 if (empty($_POST) == false) {
 
   $nombre = $_POST["nombre"];
-  echo "<p>Hola $nombre </p>";
+ 
 
-
-if ( $_FILES["Adjuntar"]["error"] == 0) {
-  $nombreArchivo = $_FILES["Adjuntar"]["name"];
-  echo "<p>Hola el archivo es  $nombreArchivo i se ha enviado correctamente</p>";
-
-  echo "<img src='images/masv_03.jpg'>";
-
-
-}
+  $text = $_POST["text"];
+  $pesarxiu = $_FILES["Adjuntar"]["size"];
+  $email = $_POST["email"];
+  $url = $_FILES["Adjuntar"]["tmp_name"];
 
 
 if(isset($_POST["email"])) {
-  echo "Se tiene que enviar un mail!!";
+
+    if (empty($_POST["email"])) {
+        echo "<h1>S'ha de posar un email !!! </h1>";
+
+    }
+
+      elseif (empty($_POST["nombre"])) {
+          echo "<h1>S'ha de posar el nom usuariii!!!! </h1>";
+
+      }
+
+    else {
+
+
+if ( $_FILES["Adjuntar"]["error"] == 0) {
+
+  $nombreArchivo = $_FILES["Adjuntar"]["name"];
+  echo " <table class='taula' >
+    <tr>
+      <td rowspan='3' class='h-75'> <img src='images/masv_03.jpg'> </td>
+      <td><h2>Hola <strong>$nombre</strong>, </h2> <br>
+      <h4>El archivo es <i> $nombreArchivo </i> l'arxiu pesa: $pesarxiu bytes </strong> <br> i se ha enviado correctamente  a <strong>$email</strong> <br></h4> 
+      <h5><div><i> $text </i></div></h5>
+      </td>
+
+    </tr>
+    <tr>
+     <th>El enllaç de l'arxiu per compartir és <a href='$url'>$url </a></th>
+  </tr>
+</table> ";
+
+
 }
+
 
 
 } 
-
-else {
-  header("Location: upload.php");
+       
+    }
 }
-
 
 
 
